1d8c3352c8e9cfc15516afa9ab8537959353bc62
Dan <danielvediaj@gmail.com>
2020-12-15 17:32:19 +0100

    done

--- linear_regression_live-master/demo.py
+++ linear_regression_live-master/demo.py
@@ -5,18 +5,20 @@ from numpy import *
 
 # y = mx + b
 # m is slope, b is y-intercept
-def compute_error_for_line_given_points(b, m, points):
+def compute_error(b, m, points):
     totalError = 0
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
         totalError += (y - (m * x + b)) ** 2
-    return totalError / float(len(points))
+# average error    
+    return totalError/float(len(points))
 
 def step_gradient(b_current, m_current, points, learningRate):
     b_gradient = 0
     m_gradient = 0
     N = float(len(points))
+
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
@@ -41,10 +43,10 @@ def run():
     initial_b = 0 # initial y-intercept guess
     initial_m = 0 # initial slope guess
     num_iterations = 1000
-    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points)))
+    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error(initial_b, initial_m, points)))
     
     [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
-    print("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points)))
+    print("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error(b, m, points)))
 
 if __name__ == '__main__':
     run()
